5eda5cd831f8dc00aebaa645149b4a6b2386fbe9
tarun <tarunbk1947@gmail.com>
2026-03-17 00:01:22 +0530

    Initial commit

--- Book.java
+++ Book.java
@@ -1,32 +1,11 @@
-class Book {
-
-    String title;
-    String author;
-    String publisher;
-    int price;
-    int pages;
-    String language;
-    String genre;
-    int edition;
-    String isbn;
-    boolean hardcover;
-    double weight;
-    String color;
-
-    Book(String title,String author,String publisher,int price,int pages,String language,
-         String genre,int edition,String isbn,boolean hardcover,double weight,String color){
-
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.price = price;
-        this.pages = pages;
-        this.language = language;
-        this.genre = genre;
-        this.edition = edition;
-        this.isbn = isbn;
-        this.hardcover = hardcover;
-        this.weight = weight;
-        this.color = color;
-    }
-}
+class Book{
+  int BookId;
+  
+  Book(int BookId){
+     this.BookId=BookId;
+	 }
+	 
+	public void getBookDetails(){
+	   System.out.println("The book id is:" +this.BookId);
+	   }
+	}
--- Hotel.java
+++ Hotel.java
@@ -1,14 +1,24 @@
 class Hotel{
-   int HotelId;
-   Floor floor;
- 
-   
-   Hotel(){
+  int HotelId;
+  String HotelName;
+  String[] Branches;	
+  Book book;
+  
+  Hotel(int HotelId, String HotelName,String[] Branches, Book book){
      this.HotelId=HotelId;
+	 this.HotelName=HotelName;
+	 this.Branches=Branches;
+	 this.book=book;
+	}
+	
+  public void getHotelDetails(){
+	 System.out.println();
+     System.out.println("The hotel id is:" +this.HotelId);
+	 System.out.println("The hotel name is:" +this.HotelName);
+	 System.out.println("Lists of hotel branches are");
+	 for(String branches: Branches){
+		  System.out.println(branches);
 	 }
-   
-   public void getDetails(){
-      System.out.println("The hotel id is:" +this.HotelId);
-	   this.floor.getFloorDetails();
-	  }
-}
+	 this.book.getBookDetails();
+	 }
+	}

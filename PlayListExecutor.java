511b8d102e93b590a17517a783a5f7f9a98e11b2
Tarunbk648 <tarunbk1947@gmail.com>
2026-04-01 16:51:59 +0530

    Comment out song operations in PlayListExecutor
    Comment out song-related operations in main method.

--- PlayListExecutor.java
+++ PlayListExecutor.java
@@ -1,22 +1,26 @@
 class PlayListExecutor{
  public static void main(String[] args){
    PlayList playlist = new PlayList();
-   playlist.addSong("Ranavikrama");
-   playlist.addSong("Chuttamalle");
-   playlist.addSong("Tum Hi Ho");
+   // playlist.addSong("Ranavikrama");
+   // playlist.addSong("Chuttamalle");
+   // playlist.addSong("Tum Hi Ho");
    
    
+   // playlist.getDetails();
+   
+   // playlist.getNameByIndex(2);
+   
+   // playlist.getIndexByName("Ranavikrama");
+
+   // playlist.UpdatedName("Ranavikrama", "Gajakesari");
+
+   // playlist.deletePlaylist("Gajakesari");
+
    playlist.getDetails();
-   
-   playlist.getNameByIndex(2);
-   
-   playlist.getIndexByName("Ranavikrama");
 
-   playlist.UpdatedName("Ranavikrama", "Gajakesari");
-
-   playlist.deletePlaylist("Gajakesari");
-
-   playlist.getDetails();
+   String[] songs={"Ranavikrama", "Chuttamelle", "Tum Hi Ho"};
+   boolean songNames=playlist.createSongNames(songs);
+   System.out.println("Song names:" +songNames);
    
   }
  }

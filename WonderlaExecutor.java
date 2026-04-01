aed1a1e59f0637160080d908efe88c9c9ea7e791
Tarunbk648 <tarunbk1947@gmail.com>
2026-04-01 17:06:36 +0530

    Update WonderlaExecutor.java

--- WonderlaExecutor.java
+++ WonderlaExecutor.java
@@ -1,20 +1,23 @@
 class WonderlaExecutor{
  public static void main(String[] args){
     Wonderla wonderla = new Wonderla();
-	wonderla.addWaterGames("Wave pool");
-	wonderla.addWaterGames("Lazy River");
-	wonderla.addWaterGames("Rain Disco");
+	// wonderla.addWaterGames("Wave pool");
+	// wonderla.addWaterGames("Lazy River");
+	// wonderla.addWaterGames("Rain Disco");
 	
-	wonderla.getWaterGames();
+	// wonderla.getWaterGames();
 	
-	wonderla.getNameByIndex(0);
+	// wonderla.getNameByIndex(0);
 	
-	wonderla.getIndexByName("Rain Disco");
+	// wonderla.getIndexByName("Rain Disco");
 
-	wonderla.updateAppName("Rain Disco", "Disco Rain");
+	// wonderla.updateAppName("Rain Disco", "Disco Rain");
 
-	wonderla.deleteGames("Lazy River");
+	// wonderla.deleteGames("Lazy River");
 
+	String[] games={"Wave pool", "Lazy River", "Raino Disco"};
+	 boolean waterGames=wonderla.createGamesNames(games);
+	 System.out.println(waterGames);
 	wonderla.getWaterGames();
 	}
  }

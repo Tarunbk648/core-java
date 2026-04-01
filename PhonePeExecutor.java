78f7ec20b6f2536bfa11573b0a9ce82432b3b45e
Tarunbk648 <tarunbk1947@gmail.com>
2026-04-01 16:46:00 +0530

    Refactor PhonePeExecutor to comment out operations
    Comment out existing insurance operations and add a new method to create insurance names.

--- PhonePeExecutor.java
+++ PhonePeExecutor.java
@@ -1,21 +1,25 @@
 class PhonePeExecutor{
  public static void main(String[] args){
    PhonePe phonepe = new PhonePe();
-   phonepe.addInsurance("Health Insurance");
-   phonepe.addInsurance("Bike Insurance");
-   phonepe.addInsurance("Car Insurance");
+   // phonepe.addInsurance("Health Insurance");
+   // phonepe.addInsurance("Bike Insurance");
+   // phonepe.addInsurance("Car Insurance");
 
    
+   // phonepe.getInsurance();
+   
+   // phonepe.getNameByIndex(0);
+   
+   // phonepe.getIndexByName("Bike Insurance");
+
+   // phonepe.UpdateName("Health Insurance", "Plane Insurance");
+
+   // phonepe.deleteName("Bike Insurance");
+
    phonepe.getInsurance();
-   
-   phonepe.getNameByIndex(0);
-   
-   phonepe.getIndexByName("Bike Insurance");
 
-   phonepe.UpdateName("Health Insurance", "Plane Insurance");
-
-   phonepe.deleteName("Bike Insurance");
-
-   phonepe.getInsurance();
+  String[] insurance={"Health Insurance", "Bike Insurance", "Car Insurance"};
+  boolean insuranceNames=phonepe.createInsuranceNames(insurance);
+  System.out.println("Insurance names:" +insuranceNames);
    }
   }

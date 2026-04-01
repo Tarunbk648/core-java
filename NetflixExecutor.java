e040f009d22f037f7a2683b84a040a156d2132d8
Tarunbk648 <tarunbk1947@gmail.com>
2026-04-01 16:39:35 +0530

    Refactor NetflixExecutor to use createNetflixNames
    Comment out existing web series methods and add new functionality to create Netflix names.

--- NetflixExecutor.java
+++ NetflixExecutor.java
@@ -1,20 +1,24 @@
 class NetflixExecutor{
  public static void main(String[] args){
     Netflix netflix = new Netflix();
-	netflix.addWebSeries("Stranger Things");
-	netflix.addWebSeries("Money Heist");
-	netflix.addWebSeries("Dark");
+	// netflix.addWebSeries("Stranger Things");
+	// netflix.addWebSeries("Money Heist");
+	// netflix.addWebSeries("Dark");
 
-	netflix.getWebSeries();
+	// netflix.getWebSeries();
 	
-	netflix.getWebSeriesNames(0);
+	// netflix.getWebSeriesNames(0);
 	
-	netflix.getWebSeriesIndex("Stranger Things");
+	// netflix.getWebSeriesIndex("Stranger Things");
 
-	netflix.UpdatedName("Dark", "White");
+	// netflix.UpdatedName("Dark", "White");
 
-	netflix.deleteName("Dark");
+	// netflix.deleteName("Dark");
 
-	netflix.getWebSeries();
+	// netflix.getWebSeries();
+
+	 String[] webseries={"Stranger things", "Money Heist", "Dark"};
+	 boolean series=netflix.createNeflixNames(webseries);
+	 System.out.println(series);
 	}
  }

e94a08cb76d0c00b2b40a21827896d4e9ce9ec24
tarun <tarunbk1947@gmail.com>
2026-04-07 00:55:10 +0530

    initial commit

--- UserAccount.java
+++ UserAccount.java
@@ -1,53 +1,46 @@
-class UserAccount{
- private int id;
- private String userName;
- private String email;
- private String password;
- private long phone;
- private String address;
- private boolean isActive;
- 
- public void setId(int id){
-   this.id=id;
-  }
- public int getId(){
-  return this.id;
-  }
- public void setUserName(String userName){
-   this.userName=userName;
+public class UserAccount{
+  private int Id;
+  private String userName;
+  private String email;
+  private String password;
+  private String phoneNumber;
+  private String gender;
+  
+  public void setId(int Id){
+    this.Id=Id;
+	}
+  public int getId(){
+   return this.Id;
    }
+  public void setUserName(String userName){
+     this.userName=userName;
+	 }
   public String getUserName(){
-     return this.userName;
-	 }
- public void setEmail(String email){
+    return this.userName;
+	}
+  public void setEmail(String email){
     this.email=email;
- }
- public String getEmail(){
-     return this.email;
-	 }
- public void setPassword(String password){
+	}
+  public String getEmail(){
+    return this.email;
+	}
+  public void setPassword(String password){
     this.password=password;
 	}
- public String getPassword(){
-     return this.password;
+  public String getPassword(){
+    return this.password;
+	}
+  public void setPhoneNumber(String phoneNumber){
+    this.phoneNumber=phoneNumber;
+	}
+  public String getPhoneNumber(){
+     return this.phoneNumber;
 	 }
- public void setPhone(long phone){
-     this.phone=phone;
-	}
- public long getPhone(){
-    return this.phone;
-	}
- public void setAddress(String address){
-    this.address=address;
-	}
- public String getAddress(){
-   return this.address;
-   }
-   public void setActive(boolean isActive){
-      this.isActive=isActive;
-	  }
-   public boolean getActive(){
-       return this.isActive;
-	   }
+  public void setGender(String gender){
+     this.gender=gender;
 	 }
-      
+  public String getGender(){
+    return this.gender;
+	}
+ }
+  
